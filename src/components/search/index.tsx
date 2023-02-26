f13349e50f9dac2fde2802c319fcee77b145a834
linus-stengard-dooer <linus.stengard+git@dooer.com>
2023-02-26 21:56:49 +0100

    🔥 Improve search results list view

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -13,6 +13,12 @@ const debounce = (fn: Function, ms = 600) => {
   };
 };
 
+const truncate = (str: string, length = 300) => {
+  if (str.length > length) {
+    return str.slice(0, length) + '…'
+  } else return str;
+}
+
 interface IMovie {
   poster_path: string
   adult: boolean
@@ -32,31 +38,39 @@ interface IMovie {
 
 const Search: React.FC = () => {
   const { data: apiData } = useApiConfig()
-  const [search, setSearch] = React.useState("interstellar")
+  const [search, setSearch] = React.useState("")
+
   const [{ loading, error, data }] = useAxios({
     url: `https://api.themoviedb.org/3/search/movie?query=${search}`,
     headers: {
       authorization: `Bearer ${environment.ACCESS_TOKEN}`
     }
   })
-  const debouncedSetSearch = (e: React.ChangeEvent<HTMLInputElement>) => debounce(setSearch)(e.currentTarget.value)
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => setSearch(e.target.value)
+  const debouncedSetSearch = React.useMemo(
+    () => debounce(changeHandler),
+    [search]
+  )
 
   return (
     <div>
-      <h1>Search movie</h1>
-      <input onChange={debouncedSetSearch} type="text" />
-      <pre>Query: {search}</pre>
-      {(data?.results ?? []).map((movie: IMovie) => (
-        <div style={{ border: '1px solid #ccc', display: 'flex', flexFlow: 'row nowrap' }} key={movie.id}>
-          {apiData && movie.poster_path ? (
+      <input className="searchInput" onChange={debouncedSetSearch} type="search" placeholder="Search for a movie…" />
+      {loading ? (
+        'Loading…'
+      ) : error ? (
+        `Error searching for ${search}`
+      ) : search != '' && (data?.results ?? []).length > 0 ? (data?.results ?? []).map((movie: IMovie) => (
+        <div className="movie-row" key={movie.id}>
+          {(apiData && movie.poster_path) ? (
             <img src={`${apiData?.images?.base_url}${apiData?.images?.poster_sizes?.[0]}${movie.poster_path}`} />
-          ) : null}
+          ) : <img src="https://via.placeholder.com/92x138?text=" />}
           <div>
             <h3>{movie.original_title} ◎ {movie.vote_average}/10 with {movie.vote_count} votes</h3>
-            <p>{movie.overview}</p>
+            <p>{truncate(movie.overview)}</p>
           </div>
         </div>
-      ))}
+      )) : null}
     </div>
   )
 }
