4fa003b3fc3f811d62d8975da6c09e32e8731e3a
linus-stengard-dooer <linus.stengard+git@dooer.com>
2023-02-26 22:16:24 +0100

    🔥 Tweak footer styling

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -7,9 +7,10 @@ import './footer.css'
 const Footer: React.FC = () => (
   <footer>
     <h5>Powered by</h5>
-    <a href="" target="_blank" rel='noreferrer noopener'>
+    <a href="https://www.themoviedb.org" target="_blank" rel='noreferrer noopener'>
       <img src={TMDBLogo} alt="The movie database logo" />
     </a>
+    <p>ReelRadar uses the TMDb API but is not endorsed or certified by TMDb.</p>
   </footer>
 )
 
